9ab0afdddd22487a4ce9a5cdf3698206f2339cdb
Alexandru Văleanu <valeanu_alex@yahoo.com>
2016-08-14 18:18:53 +0300

    DisjointSet

---
+++ Java/DisjointSet.java
@@ -0,0 +1,48 @@
+class DisjointSet {
+    private final int[] father;
+    private final int[] rank;
+    private final int N;
+
+    DisjointSet(int N){
+        this.N = N;
+        rank = new int[N + 1];
+        father = new int[N + 1];
+
+        for (int i = 1; i <= N; i++)
+            initialize(i);
+    }
+
+    void initialize(int node){
+        if (!(1 <= node && node <= N)) throw new AssertionError();
+        father[node] = node;
+        rank[node] = 1;
+    }
+
+    int find(int node){
+        if (!(1 <= node && node <= N)) throw new AssertionError();
+
+        if (father[node] == node)
+            return node;
+        else
+            return father[node] = find(father[node]);
+    }
+
+    void union(int x, int y){
+        x = find(x);
+        y = find(y);
+
+        if (x != y){
+            if (rank[x] < rank[y])
+                father[x] = y;
+            else
+                father[y] = x;
+
+            if (rank[x] == rank[y])
+                rank[x]++;
+        }
+    }
+
+    boolean connected(int x, int y){
+        return find(x) == find(y);
+    }
+}
