5aa51e3c0a248886b62bea3b79bf15eddb0ca021
Alexandru Văleanu <valeanu_alex@yahoo.com>
2016-08-20 16:58:08 +0300

    Graph

---
+++ ACM_OOP_classes/Graph.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Graph
+{
+private:
+
+    class Edge
+    {
+    public:
+
+        int node;
+        size_t next;
+
+        Edge(int _node, size_t _next) : node(_node), next(_next){
+        }
+    };
+
+    vector<Edge> graph;
+    vector<int> head;
+    int N;
+
+public:
+
+    static const int NIL;
+
+    Graph(int _N) : graph(), head(), N(_N){
+        head.resize(N, NIL);
+    }
+
+    void addEdge(int x, int y)
+    {
+        assert(1 <= x && x <= N);
+        x--;
+
+        graph.push_back(Edge(y, head[x]));
+        head[x] = graph.size() - 1;
+    }
+
+    void addDoubleEdge(int x, int y)
+    {
+        addEdge(x, y);
+        addEdge(y, x);
+    }
+
+    int getHead(int node) const
+    {
+        assert(1 <= node && node <= N);
+        return head[node - 1];
+    }
+
+    int getNeighbour(size_t p) const
+    {
+        assert(p < graph.size());
+        return graph[p].node;
+    }
+
+    int getNext(size_t p) const
+    {
+        assert(p < graph.size());
+        return graph[p].next;
+    }
+};
+
+const int Graph::NIL = -1;
+
+int main()
+{
+    return 0;
+}
