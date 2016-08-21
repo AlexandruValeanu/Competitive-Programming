9a3ecd5ceb49e5c6db9e7cd786f46b20e93a3c62
Alexandru Văleanu <valeanu_alex@yahoo.com>
2016-08-21 11:56:12 +0300

    Update Graph.java

--- Java/Graph.java
+++ Java/Graph.java
@@ -11,6 +11,7 @@ class Graph{
 
     final static int NIL = -1;
     private int[] head;
+    private int[] degree;
     private ArrayList<Edge> graph;
 
     private int N;
@@ -19,41 +20,56 @@ class Graph{
     Graph(int N){
         initialize(N);
     }
-    
+
     public int getN() {
         return N;
     }
 
     private void initialize(final int N){
         head = new int[N];
+        degree = new int[N];
         graph = new ArrayList<>();
 
         this.N = N;
         this.counter = 0;
 
         Arrays.fill(head, NIL);
+        Arrays.fill(degree, 0);
     }
 
     void addEdge(int x, int y){
-        assert 1 <= x && x <= N;
+        if (!(1 <= x && x <= N)) throw new AssertionError();
         x--; y--;
         graph.add(new Edge(y, head[x]));
         head[x] = counter++;
+        degree[x]++;
     }
 
     int getHead(int node){
-        assert 1 <= node && node <= N;
+        if (!(1 <= node && node <= N)) throw new AssertionError();
         node--;
         return head[node];
     }
 
     int getNext(int p){
-        assert 0 <= p && p < counter;
+        if (!(0 <= p && p < counter)) throw new AssertionError();
         return graph.get(p).next;
     }
 
     int getNeighbour(int p){
-        assert 0 <= p && p < counter;
+        if (!(0 <= p && p < counter)) throw new AssertionError();
         return graph.get(p).node + 1;
+    }
+
+    boolean isEmpty(int node){
+        if (!(1 <= node && node <= N)) throw new AssertionError();
+        node--;
+        return head[node] == NIL;
+    }
+
+    int size(int node){
+        assert 1 <= node && node <= N;
+        node--;
+        return degree[node];
     }
 }
