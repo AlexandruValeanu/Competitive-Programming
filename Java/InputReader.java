b8d443ea5936af8641de04b5c82a8155a3d7d7a9
Alexandru Văleanu <valeanu_alex@yahoo.com>
2016-08-27 21:42:56 +0300

    Update InputReader.java

--- Java/InputReader.java
+++ Java/InputReader.java
@@ -1,12 +1,14 @@
 class InputReader{
     private BufferedReader reader;
     private StringTokenizer tokenizer;
-    
+    private InputStream inputStream;
+
     InputReader(InputStream stream) {
-        reader = new BufferedReader(new InputStreamReader(stream), 65536);
+        reader = new BufferedReader(new InputStreamReader(stream), 1 << 20);
         tokenizer = null;
+        inputStream = stream;
     }
-    
+
     private String nextToken() {
         while (tokenizer == null || !tokenizer.hasMoreTokens()){
             try {
@@ -16,15 +18,19 @@ class InputReader{
                 throw new RuntimeException(e);
             }
         }
-    
+
         return  tokenizer.nextToken();
     }
-    
+
     int nextInt() {
         return Integer.parseInt(nextToken());
     }
-    
+
     String nextString(){
         return nextToken();
+    }
+
+    void close() throws IOException {
+        inputStream.close();
     }
 }
